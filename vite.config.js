d76180c462cdc7cbe02c9dcb33ea43d80e17984e
kaviyakumar23 <kaviyakumar23@gmail.com>
2025-01-27 23:32:55 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,16 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
     },
-  },
+    define: {
+      __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    },
+  };
 });
